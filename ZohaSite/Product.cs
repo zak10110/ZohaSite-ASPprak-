20688f1f734a6d1d7c3eb688c825baa8f52d803f
zak10110 <35228898+zak10110@users.noreply.github.com>
2022-06-21 03:25:47 +0300

    ADD Cards and trying to Add StatickList

---
+++ ZohaSite/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace ZohaSite
+{
+    public class Productc
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Price { get; set; }
+        public string Img { get; set; }
+
+        public Productc(int id, string name, string price, string img)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Img = img;
+        }
+    }
+}
---
+++ ZohaSite/ProductList.cs
@@ -0,0 +1,16 @@
+﻿namespace ZohaSite
+{
+    public static class ProductList
+    {
+        public static List<Productc> productcs { get; set; }=new List<Productc>();
+
+        public static void AddList(List<Productc> productc)
+        {
+
+            productcs = productc;
+        
+        
+        }
+
+    }
+}
